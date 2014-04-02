4508b5b55b761ed2724c63af31111c44724396c5
albertogg <albertogrespan@gmail.com>
2014-04-02 16:30:10 -0430

    Add a before_create callback to generate the hash
    This before create will generate the URL hash used as the "shortlink"

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,6 @@
 class Link < ActiveRecord::Base
+  before_create :set_uri_hash
+
   def self.by_ids(params)
     if params.present?
       where(id: params.split(','))
@@ -10,12 +12,14 @@ class Link < ActiveRecord::Base
   def self.create_new_link(params)
     link = Link.new
     link.uri = params['uri']
-    link.uri_hash = params['uri_hash']
-    link.viewed = params['viewed']
     link
   end
 
   def href
     "#{ENV['URL']}/#{uri_hash}"
+  end
+
+  def set_uri_hash
+    self.uri_hash = rand(36**5).to_s(36)
   end
 end
